a8e56df8e29b043c891c25676b3e0cc5b6c427e0
Phillip Mantatsky <pmantatsky1@bsd220.org>
2023-10-24 09:46:40 -0500

    Initial Commit

---
+++ Weekly Payroll/Weekly_PayrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weekly_PayrollApp.swift
+//  Weekly Payroll
+//
+//  Created by Phillip Mantatsky on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Weekly_PayrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
